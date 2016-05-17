be5345df2aeaf79aea5cf6b3d842ce4b57fed08f
Erozbliz <erozbliz@hotmail.com>
2016-05-17 15:25:25 +0200

    okok

--- app/modules/not-connected/inscription.php
+++ app/modules/not-connected/inscription.php
@@ -6,20 +6,6 @@
   }
 ?>
 <!DOCTYPE html>
-<!--
-  Material Design Lite
-  Copyright 2015 Google Inc. All rights reserved.
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-      https://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License
--->
-
 <html lang="fr">
 <head>
   <meta charset="utf-8">
@@ -161,7 +147,7 @@
         </main>
 		
 		<fieldset class="form-group">
-                              <label for="exampleSelect1">Pays</label>
+                              <label for="exampleSelect1">Pays</label><br>
                               <select class="form-control" id="exampleSelect1" name="selectPays">
                                   <option value="AF">Afghanistan</option>
                                   <option value="AX">Åland Islands</option>
@@ -416,7 +402,7 @@
                            </fieldset>
 	  
 		<fieldset class="form-group">
-                              <label for="exampleSelect1">Occupation</label>
+                              <label for="exampleSelect1">Occupation</label><br>
                               <select class="form-control" id="exampleSelect2" name="selectJob">
                                  <option value="Etudiant">Etudiant</option>
                                  <option value="En recherche d'emploi">En recherche d'emploi</option>
