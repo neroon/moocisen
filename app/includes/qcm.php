4007e109e03889af6c7132c9c35e31dbcbdb63f2
Erozbliz <erozbliz@hotmail.com>
2016-05-20 11:15:17 +0200

    qcm maj bug doublon

--- app/includes/qcm.php
+++ app/includes/qcm.php
@@ -1,11 +1,9 @@
 <?php
-
 	function exoQcm($idMooc,$idChap,$idExo,$bdd,$numeroExo)
 	{
 		try{
 			$selectChap = $bdd->prepare("SELECT * FROM chapitre WHERE id_mooc = $idMooc");
 			$selectChap->execute();
-
 			$lignesChap = $selectChap->fetchAll();
 			
 			$selectExo = $bdd->prepare("SELECT * FROM exercice WHERE id_chapitre = $idChap");
@@ -18,7 +16,6 @@
 			$selectqcm->execute();
 			
 			$lignesQcm = $selectqcm->fetchAll();
-
 			//Reponse du qcm
 			$tabHint = array();
 			$tabSolution = array();
@@ -30,7 +27,6 @@
 				$solution = $lignesQcm[$i]["solution"];
 				$solution=htmlentities($solution, ENT_QUOTES, "UTF-8");
 				$tabHint = preg_split('[-]', $solution);
-
 				for($itab = 0; $itab < sizeof($tabHint) ; $itab++)
 				{
 					//var_dump($tabHint);
@@ -40,15 +36,14 @@
 					//obligatoire si plusieurs reponse de meme nom
 					$lowercase = strtolower($tabHint[$itab]);
 					if($lowercase == 'oui'){
-						$tabSolution[$numCaseTab]=$tabHint[$itab].$i; //rajoute $i si doublon
-
+						$tabSolution[$numCaseTab]=$tabHint[$itab].$i.$idExo; //rajoute $i si doublon
 						//print_r("+*".$tabSolution[$numCaseTab]);
 					}else if($lowercase == 'non'){
-						$tabSolution[$numCaseTab]=$tabHint[$itab].$i; //rajoute $i si doublon
+						$tabSolution[$numCaseTab]=$tabHint[$itab].$i.$idExo; //rajoute $i si doublon
 						//print_r("-*".$tabSolution[$numCaseTab]);
 					}
 					else{
-						$tabSolution[$numCaseTab]=$tabHint[$itab].$i;
+						$tabSolution[$numCaseTab]=$tabHint[$itab].$i.$idExo;
 						//print_r("-*".$tabSolution[$numCaseTab]);
 					}
 						
@@ -59,9 +54,9 @@
 			//Type hidden qui correpond au reponse pour le qcm
 			//$temp = $tabSolution;
 			$temp = preg_replace("/\s+/","", $tabSolution);
+			$temp = str_replace( ',', '', $temp );
 			//$temp = preg_replace("/é/","xxx", $temp );
 			//print_r($temp);
-
 			//$tabSolution=implode(",",$tabSolution);
 			//$tabSolution=json_encode($tabSolution);
 			//echo 'tab réponse='.$tabSolution;
@@ -78,7 +73,6 @@
 			echo "<input type='hidden' id='ide' name='ide' value='".$idExo."'/>";
 			//echo "<input type='hidden' id='indice' class='".$idExo."' name='indice' value='".htmlspecialchars(stripslashes($lignesQcm[0]["indice_qcm"]))."'/>";//indice
 			echo '<input type="hidden" id="indice" class="'.$idExo.'" name="indice" value="' . htmlspecialchars(stripslashes($lignesQcm[0]["indice_qcm"])) . '" />';
-
 			//affichage du QCM
 				$ouinon = 0;
 			for($i = 0; $i < sizeof($lignesQcm) ; $i++)
@@ -87,7 +81,6 @@
 				$tab = array();
 				$tab = preg_split('[-]', $reponse);
 				//var_dump($lignesExo);
-
 				 echo'<div class="form-group">
 					<h2 class="StepTitle center">'.$lignesQcm[$i]["question"].'</h2><br>';
 				
@@ -99,7 +92,7 @@
 										{
 										echo'<div class="ck-button">
 											   <label>
-												  <input type="checkbox" name="'.$tab[$itab].$i.'" value="" style="display:none;"><span>'.$tab[$itab].'</span> 
+												  <input type="checkbox" name="'.$tab[$itab].$i.$idExo.'" value="" style="display:none;"><span>'.$tab[$itab].'</span> 
 											   </label>
 											</div>';
 										}
@@ -107,23 +100,27 @@
 										{
 										echo'<div class="ck-button">
 											   <label>
-												  <input type="checkbox"name="'.$tab[$itab].$i.'" value="" style="display:none;"><span>'.$tab[$itab].'</span> 
+												  <input type="checkbox"name="'.$tab[$itab].$i.$idExo.'" value="" style="display:none;"><span>'.$tab[$itab].'</span> 
 											   </label>
 											</div><br>';
 										}
 									}
 									else{
-										$affichageetvalue = preg_replace("/\s+/","",$tab[$itab]);
+
+										$affichageetvalue=htmlspecialchars(stripslashes($tab[$itab]));
+										$affichageetvalue=htmlentities($affichageetvalue, ENT_QUOTES, "UTF-8");
+										$affichageetvalue = preg_replace("/\s+/","",$affichageetvalue);
+										$affichageetvalue = str_replace( ',', '',$affichageetvalue);
+
 										echo '<div class="checkbox center">
 											<label class="hover">
-												<div class="icheckbox_flat-green checked hover" style="position: relative;"><input type="checkbox" name="'.$affichageetvalue.$i.'" class="flat"  style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"></ins></div> 
+												<div class="icheckbox_flat-green checked hover" style="position: relative;"><input type="checkbox" name="'.$affichageetvalue.$i.$idExo.'" class="flat"  style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"></ins></div> 
 												'.$tab[$itab].'<br>
 											</label>
 										</div>';
 										//echo 'sok';
 									}										
 								}
-
 				echo' </div><br>';
 				echo '<button type="button" class="myindice btn btn-round btn-success btn-xs" value="'.$idExo.'">Indice</button>'; // indice
 				//}
